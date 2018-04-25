995320787fd428f698da004a58c50b994b3344e6
Dmitry-Vasilyev <dimav.programming@gmail.com>
2018-04-25 13:46:18 +0300

    NEW Exercise PULL FETCH REMOTE

---
+++ src/git/exercises/Main.java
@@ -0,0 +1,9 @@
+package git.exercises;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Work work = new Work();
+        work.doWork(5);
+    }
+}
---
+++ src/git/exercises/Work.java
@@ -0,0 +1,10 @@
+package git.exercises;
+
+public class Work {
+    public void doWork(int n) {
+        while (n > 0) {
+            System.out.print(n--);
+        }
+        System.out.println();
+    }
+}
